f246827d99a17fb87c9ade7aa17ad0dbffb27ec8
sambit-giri <sambit.giri@gmail.com>
2025-01-03 17:51:11 +0100

    dropout, weight decay implemented properly

--- src/emulator/neural_networks.py
+++ src/emulator/neural_networks.py
@@ -235,175 +235,170 @@ def metric_function(y_true, y_pred, metric='r2'):
 
     if type(metric)==str:
         assert metric in ['explained_variance_score', 'max_error', 
-                          'mean_squared_error', 'mean_squared_log_error',
+                          'mean_squared_error', 'mse', 'mean_squared_log_error',
                           'mean_absolute_error', 'median_absolute_error',
                           'r2_score', 'r2']
 
         if metric=='explained_variance_score': metric = metrics.explained_variance_score
         if metric=='max_error': metric = metrics.max_error
-        if metric=='mean_squared_error': metric = metrics.mean_squared_error
+        if metric==['mean_squared_error','mse']: metric = metrics.mean_squared_error
         if metric=='mean_squared_log_error': metric = metrics.mean_squared_log_error
         if metric=='median_absolute_error': metric = metrics.median_absolute_error
         if metric in ['r2', 'r2_score']: metric = metrics.r2_score
 
     return metric(y_true, y_pred)
+    
+def numpy_to_tensor(array):
+    return torch.tensor(array, dtype=torch.float32)
 
+def tensor_to_numpy(tensor):
+    return tensor.detach().cpu().numpy()
+    
 class NNRegressor:
-    def __init__(self, model=None, layers=[3,32,64,128,250],
+    def __init__(self, layers=[3, 32, 64, 16],
                  dropout_prob=0.5, weight_decay=0,
                  optimizer='Adam', loss_fn='MSE',
-                 learning_rate=1e-4,
-                 ):
-        if model is None:
-            layer_modules = []
-            for i in range(len(layers) - 1):
-                layer_modules.append(nn.Linear(layers[i], layers[i+1]))
-                if i < len(layers) - 2:
-                    layer_modules.append(nn.ReLU())
-            model = nn.Sequential(*layer_modules)
-
-        self.model = model 
-        self.X_min = None
-        self.X_max = None
-        self.y_min = None
-        self.y_max = None
-        self.n_epochs = None
-        self.batch_size = None
-        self.lr = None
-        self.loss_history = None
+                 learning_rate=1e-4, validation_size=0.2):
+        self.layers = layers
         self.dropout_prob = dropout_prob
         self.weight_decay = weight_decay
-        self.optimizer = optimizer
-        self.loss_fn = loss_fn
+        self.optimizer_name = optimizer
+        self.loss_fn_name = loss_fn
         self.learning_rate = learning_rate
+        self.validation_size = validation_size
+        
+        # Model and normalization parameters
+        self.model = self.build_model(layers, dropout_prob)
+        self.X_min, self.X_max = None, None
+        self.y_min, self.y_max = None, None
+        
+        # Tracking losses and additional data
+        self.loss_history = []
+        self.extra_data = None
+        
+        # PCA attributes
+        self.pca = None
+        self.pca_n_components = None
 
-    def numpy_to_tensor(self, data):
-        return torch.tensor(data, dtype=torch.float32)
-    
-    def tensor_to_numpy(self, data):
-        return data.numpy()
-    
-    def normalise(self, data):
-        data_min = data.min(axis=0)
-        data_max = data.max(axis=0)
-        data_normed = (data-data_min)/(data_max-data_min)
-        return {'min': data_min, 'max': data_max, 'normed': data_normed}
-    
-    def apply_dropout(self):
-        if self.dropout_prob > 0:
-            for layer in self.model.modules():
-                if isinstance(layer, nn.Dropout):
-                    layer.p = self.dropout_prob
+        # Training state
+        self.current_epoch = 0
+        self.optimizer_state = None
 
-    def apply_weight_decay(self):
-        if self.weight_decay > 0:
-            for param in self.model.parameters():
-                param.data -= self.weight_decay * param.data
+    def build_model(self, layers, dropout_prob):
+        modules = []
+        for i in range(len(layers) - 1):
+            modules.append(nn.Linear(layers[i], layers[i + 1]))
+            if i < len(layers) - 2:  # Exclude activation and dropout for the output layer
+                modules.append(nn.ReLU())
+                if dropout_prob > 0:
+                    modules.append(nn.Dropout(p=dropout_prob))
+        return nn.Sequential(*modules)
 
-    def fit(self, X, y, n_epochs=100, batch_size=10, learning_rate=None):
+    def fit(self, X, y, n_epochs=100, batch_size=10, learning_rate=None, continue_training=False):
         self.n_epochs = n_epochs
         self.batch_size = batch_size
         if learning_rate is not None:
-            self.learning_rate = learning_rate 
+            self.learning_rate = learning_rate
 
-        self.X_min, self.X_max = X.min(axis=0), X.max(axis=0)
-        self.y_min, self.y_max = y.min(axis=0), y.max(axis=0)
-        X_normed = (X-self.X_min)/(self.X_max-self.X_min)
-        y_normed = (y-self.y_min)/(self.y_max-self.y_min)
+        # Normalize data
+        if not continue_training:
+            self.X_min, self.X_max = X.min(axis=0), X.max(axis=0)
+            self.y_min, self.y_max = y.min(axis=0), y.max(axis=0)
+        X_normed = (X - self.X_min) / (self.X_max - self.X_min)
+        y_normed = (y - self.y_min) / (self.y_max - self.y_min)
 
+        # Split data into training and test sets
+        train_size = 1.0 - self.validation_size
+        X_train, X_test, y_train, y_test = train_test_split(X_normed, y_normed, train_size=train_size, shuffle=True)
+        X_train, y_train = numpy_to_tensor(X_train), numpy_to_tensor(y_train)
+        X_test, y_test = numpy_to_tensor(X_test), numpy_to_tensor(y_test)
 
-        X_train, X_test, y_train, y_test = train_test_split(
-                X_normed, y_normed, train_size=0.8, shuffle=True)
-        X_train = self.numpy_to_tensor(X_train)
-        y_train = self.numpy_to_tensor(y_train)
-        X_test = self.numpy_to_tensor(X_test)
-        y_test = self.numpy_to_tensor(y_test)
+        # Loss function
+        loss_fn = nn.MSELoss() if self.loss_fn_name.lower() == 'mse' else self.loss_fn_name
 
-        model = self.model
-        batch_start = torch.arange(0, len(X_train), batch_size)
+        # Optimizer
+        optimizer = self.get_optimizer(self.optimizer_name, self.learning_rate, self.weight_decay)
+        if continue_training and self.optimizer_state is not None:
+            optimizer.load_state_dict(self.optimizer_state)
 
-        # loss function and optimizer
-        if self.loss_fn.lower()=='mse':
-            loss_fn = nn.MSELoss()  # mean square error
+        # Training loop
+        best_loss = float('inf') if not continue_training else self.loss_history[-1]
+        best_model_state = None if not continue_training else self.model.state_dict()
+
+        # Progress bar with tqdm
+        progress_bar = tqdm(range(self.current_epoch, self.current_epoch + n_epochs), desc="Training Epochs")
+
+        for epoch in progress_bar:
+            self.model.train()
+            epoch_loss = 0.0
+
+            # Batch training
+            indices = torch.randperm(X_train.size(0))
+            for i in range(0, X_train.size(0), batch_size):
+                batch_indices = indices[i:i + batch_size]
+                X_batch, y_batch = X_train[batch_indices], y_train[batch_indices]
+
+                # Forward pass
+                y_pred = self.model(X_batch)
+                loss = loss_fn(y_pred, y_batch)
+
+                # Backward pass
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+                epoch_loss += loss.item()
+
+            # Evaluation on validation data
+            self.model.eval()
+            with torch.no_grad():
+                y_pred = self.model(X_test)
+                test_loss = loss_fn(y_pred, y_test).item()
+
+            self.loss_history.append(test_loss)
+            if test_loss < best_loss:
+                best_loss = test_loss
+                best_model_state = self.model.state_dict()
+
+            # Update tqdm description
+            progress_bar.set_description(f"Epoch {epoch + 1}/{n_epochs}, Val Loss: {test_loss:.2e}")
+
+        # Update state
+        self.current_epoch += n_epochs
+        self.optimizer_state = optimizer.state_dict()
+
+        # Load the best model weights
+        self.model.load_state_dict(best_model_state)
+
+    def get_optimizer(self, optimizer_name, learning_rate, weight_decay):
+        if optimizer_name == 'Adam':
+            return optim.Adam(self.model.parameters(), lr=learning_rate, weight_decay=weight_decay)
+        elif optimizer_name == 'RMSprop':
+            return optim.RMSprop(self.model.parameters(), lr=learning_rate, weight_decay=weight_decay)
+        elif optimizer_name == 'Adagrad':
+            return optim.Adagrad(self.model.parameters(), lr=learning_rate, weight_decay=weight_decay)
         else:
-            loss_fn = self.loss_fn
-
-        if self.optimizer=='Adam':
-            optimizer = optim.Adam(self.model.parameters(), lr=self.learning_rate)
-        elif self.optimizer=='RMSprop':
-            optimizer = optim.RMSprop(self.model.parameters(), lr=self.learning_rate)
-        elif self.optimizer=='Adagrad':
-            optimizer = optim.Adagrad(self.model.parameters(), lr=self.learning_rate)
-        else:
-            optimizer = self.optimizer
-
-        # Hold the best model
-        best_mse = np.inf   # init to infinity
-        best_weights = None
-        history = []
-        
-        # training loop
-        for epoch in range(n_epochs):
-            model.train()
-            with tqdm(batch_start, unit="batch", mininterval=0) as bar:
-                bar.set_description(f"Epoch {epoch}")
-                for start in bar:
-                    # take a batch
-                    X_batch = X_train[start:start+batch_size]
-                    y_batch = y_train[start:start+batch_size]
-                    # forward pass
-                    y_pred = model(X_batch)
-                    loss = loss_fn(y_pred, y_batch)
-                    # backward pass
-                    optimizer.zero_grad()
-                    loss.backward()
-                    # update weights
-                    optimizer.step()
-                    # print progress
-                    bar.set_postfix(mse=float(loss))
-            # evaluate accuracy at end of each epoch
-            model.eval()
-            y_pred = model(X_test)
-            mse = loss_fn(y_pred, y_test)
-            mse = float(mse)
-            history.append(mse)
-            if mse < best_mse:
-                best_mse = mse
-                best_weights = copy.deepcopy(model.state_dict())
-        
-        # restore model and return best accuracy
-        model.load_state_dict(best_weights)
-        self.model = model
-        self.loss_history = history
+            raise ValueError(f"Unsupported optimizer: {optimizer_name}")
 
     def predict(self, X):
-        X_normed = (X-self.X_min)/(self.X_max-self.X_min)
-        # Convert input data to tensor
-        X_tensor = self.numpy_to_tensor(X_normed)
-        # Set model to evaluation mode
+        X_normed = (X - self.X_min) / (self.X_max - self.X_min)
+        X_tensor = numpy_to_tensor(X_normed)
         self.model.eval()
-        # Make predictions
         with torch.no_grad():
             y_pred = self.model(X_tensor)
-        # Convert predictions to numpy array
-        y_pred_numpy = self.tensor_to_numpy(y_pred)
-        return y_pred_numpy*(self.y_max-self.y_min)+self.y_min
-    
-    def append_extra_data(self, extra_data):
-        self.extra_data = extra_data
+        return tensor_to_numpy(y_pred) * (self.y_max - self.y_min) + self.y_min
 
     def save_model(self, filepath):
         save_data = {
-                'model_state_dict': self.model.state_dict(),
-                'X_min': self.X_min,
-                'X_max': self.X_max,
-                'y_min': self.y_min,
-                'y_max': self.y_max,
-                'n_epochs': self.n_epochs,
-                'batch_size': self.batch_size,
-                'learning_rate': self.learning_rate,
-                'loss_history': self.loss_history,
-                'extra_data': self.extra_data,
+            'model_state_dict': self.model.state_dict(),
+            'X_min': self.X_min,
+            'X_max': self.X_max,
+            'y_min': self.y_min,
+            'y_max': self.y_max,
+            'loss_history': self.loss_history,
+            'extra_data': self.extra_data,
+            'pca': self.pca,
+            'pca_n_components': self.pca_n_components,
             }
         torch.save(save_data, filepath)
 
@@ -414,27 +409,27 @@ class NNRegressor:
         self.X_max = checkpoint['X_max']
         self.y_min = checkpoint['y_min']
         self.y_max = checkpoint['y_max']
-        self.n_epochs = checkpoint['n_epochs']
-        self.batch_size = checkpoint['batch_size']
-        self.learning_rate = checkpoint['learning_rate']
         self.loss_history = checkpoint['loss_history']
         self.extra_data = checkpoint['extra_data']
+        self.pca = checkpoint['pca']
+        self.pca_n_components = checkpoint['pca_n_components']
 
     def PCA_fit(self, X, n_components=32):
         from sklearn.decomposition import PCA
-        pca = PCA(n_components=n_components)
-        pca.fit(X)
-        X_transformed = pca.transform(X)
-        print(X.shape, X_transformed.shape, pca.score(X))
-        return pca 
-    
-    def PCA_transform_data(self, data, pca):
-        out = pca.transform(data)
-        return out
+        self.pca = PCA(n_components=n_components)
+        self.pca.fit(X)
+        self.pca_n_components = n_components
+        return self.pca
 
-    def PCA_inverse_transform_data(self, data, pca):
-        out = pca.inverse_transform(data)
-        return out 
+    def PCA_transform(self, X):
+        if self.pca is None:
+            raise ValueError("PCA has not been fitted. Call `PCA_fit` first.")
+        return self.pca.transform(X)
+
+    def PCA_inverse_transform(self, X_transformed):
+        if self.pca is None:
+            raise ValueError("PCA has not been fitted. Call `PCA_fit` first.")
+        return self.pca.inverse_transform(X_transformed)
     
 
 # SIREN layer definition
